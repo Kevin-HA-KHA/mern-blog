8af8157059e6451362a629734eafca48c83653c8
Kevin-HA-KHA <nivekah27@gmail.com>
2024-02-20 23:01:37 +0100

    vercel

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,6 +1,7 @@
 const apiConfig = {
     // apiUrl: 'https://mern-blog-api-wheat.vercel.app'
-    apiUrl : 'http://localhost:4000'
+    apiUrl: 'https://mern-blog-app-sandy.vercel.app/'
+    // apiUrl : 'http://localhost:4000'
 };
 
 export default apiConfig;
