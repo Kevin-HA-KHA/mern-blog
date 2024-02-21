0da78e70c53991fed2678a55f28838a3776b4461
Kevin-HA-KHA <nivekah27@gmail.com>
2024-02-21 15:54:14 +0100

    last

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,7 +1,7 @@
 const apiConfig = {
-    apiUrl: 'https://mern-blog-api-z9j3.onrender.com',
-    // appURL: 'mern-blog-app-sandy.vercel.app',
-    // apiUrl : 'http://localhost:4000',
+    // apiUrl: 'https://mern-blog-api-z9j3.onrender.com',
+    // appURL: 'http://localhost:3000',
+    apiUrl : 'http://localhost:4000',
     appURL : 'http://localhost:3000'
 };
 
