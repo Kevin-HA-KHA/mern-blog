d273581f6f395e64d423f9131dfc152d7b17cca0
Kevin-HA-KHA <nivekah27@gmail.com>
2024-02-21 16:02:57 +0100

    last

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,8 +1,8 @@
 const apiConfig = {
-    // apiUrl: 'https://mern-blog-api-z9j3.onrender.com',
-    // appURL: 'http://localhost:3000',
-    apiUrl : 'http://localhost:4000',
-    appURL : 'http://localhost:3000'
+    apiUrl: 'https://mern-blog-api-z9j3.onrender.com',
+    appURL: 'http://localhost:3000',
+    // apiUrl : 'http://localhost:4000',
+    // appURL : 'http://localhost:3000'
 };
 
 module.exports = apiConfig;
