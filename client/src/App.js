79a979cb460be1884a373eb3ff50778e50c38f2d
Kevin-HA-KHA <nivekah27@gmail.com>
2024-02-24 11:37:40 +0100

    add website title

--- client/src/App.js
+++ client/src/App.js
@@ -15,9 +15,7 @@ function App() {
     <UserContextProvider>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={
-              <IndexPage />
-          } />
+          <Route index element={<IndexPage />}/>
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
           <Route path='/create' element={<CreatePost />} />
